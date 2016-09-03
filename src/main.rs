251190a808593ec71c3dc0d8abe594c90c95b604
Matthew Piziak <matthew.piziak@gmail.com>
2016-09-03 13:52:58 -0400

    make cfg a parameter

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,15 @@ extern crate clap;
 extern crate yaml_rust;
 
 use std::collections::HashMap;
-use std::env::home_dir;
 use std::fs::File;
 use std::io::Read;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
-use clap::{App, Arg, SubCommand};
+use clap::{App, Arg};
 
 use self::yaml_rust::{Yaml, YamlLoader};
 
 mod doctrine;
-
-fn yaml_cfg_path() -> PathBuf {
-    let yaml_cfg_relative_path = "Dropbox/Log/Xup/src/xup.yaml";
-    let mut home_dir = home_dir().expect("Could not resolve home directory.");
-    home_dir.push(yaml_cfg_relative_path);
-    home_dir
-}
 
 fn file_contents(path: &Path) -> String {
     let mut file = File::open(&path).expect("Can't open file");
@@ -27,8 +19,7 @@ fn file_contents(path: &Path) -> String {
     contents
 }
 
-fn load_yaml_cfg() -> Yaml {
-    let yaml_cfg_path = yaml_cfg_path();
+fn load_yaml_cfg(yaml_cfg_path: &Path) -> Yaml {
     let file_contents = file_contents(&yaml_cfg_path);
     let mut contents = YamlLoader::load_from_str(&file_contents).expect("Couldn't load");
 
@@ -44,8 +35,9 @@ fn load_yaml_cfg() -> Yaml {
 
 type DoctrineName = String;
 
-fn load_doctrines() -> HashMap<DoctrineName, doctrine::Doctrine> {
-    let doctrines = doctrine::Doctrine::many_from_yaml(&load_yaml_cfg()).expect("Something");
+fn load_doctrines(yaml_cfg_path: &Path) -> HashMap<DoctrineName, doctrine::Doctrine> {
+    let yaml_cfg = load_yaml_cfg(&yaml_cfg_path);
+    let doctrines = doctrine::Doctrine::many_from_yaml(&yaml_cfg).expect("Something");
     doctrines.into_iter().map(|d| (d.name.clone(), d)).collect()
 }
 
@@ -58,6 +50,7 @@ fn xup(doctrine: &doctrine::Doctrine) -> String {
 }
 
 fn main() {
+
     let matches = App::new("xup")
         .about("Outputs x-up string for given doctrine")
         .arg(Arg::with_name("doctrine")
@@ -65,17 +58,18 @@ fn main() {
             .long("doctrine")
             .value_name("DOCTRINE")
             .takes_value(true))
-        .subcommand(SubCommand::with_name("ls").about("Lists available doctrines"))
+        .arg(Arg::with_name("cfg")
+            .short("c")
+            .long("cfg")
+            .value_name("CFG")
+            .takes_value(true)
+            .required(true))
         .get_matches();
 
-    let doctrines = load_doctrines();
+    let yaml_cfg_path = Path::new(matches.value_of("cfg")
+        .expect("YAML configuration file not found."));
 
-    if let Some(_) = matches.subcommand_matches("ls") {
-        for doctrine_name in doctrines.keys() {
-            println!("{}", doctrine_name);
-        }
-        return;
-    }
+    let doctrines = load_doctrines(yaml_cfg_path);
 
     match matches.value_of("doctrine") {
         Some(doctrine_name) => {
@@ -84,6 +78,11 @@ fn main() {
                 None => println!("Requested doctrine {} not found.", doctrine_name),
             }
         }
-        None => println!("No doctrine requested."),
+        None => {
+            for doctrine_name in doctrines.keys() {
+                println!("{}", doctrine_name);
+            }
+            return;
+        }
     }
 }
